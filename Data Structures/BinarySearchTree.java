9d2a199cc0b92c838d65a82d79f18b52ee233d12
varun0211 <r.varunrockz@gmail.com>
2020-08-20 01:37:27 +0530

    Update BinarySearchTree.java

--- Data Structures/BinarySearchTree.java
+++ Data Structures/BinarySearchTree.java
@@ -149,23 +149,23 @@ Note: Recursive insertion should be implemented(not yet tried)
 		BinarySearchTree tree = new BinarySearchTree();
 
 		tree.insert(50); 
-        tree.insert(30); 
-        tree.insert(20); 
-        tree.insert(40); 
-        tree.insert(70); 
-        tree.insert(60); 
-        tree.insert(80);
+        	tree.insert(30); 
+        	tree.insert(20); 
+        	tree.insert(40); 
+        	tree.insert(70); 
+        	tree.insert(60); 
+        	tree.insert(80);
 
-        // tree.inOrderTraversal(tree.root); // [20,30,40,50,60,70,80]
-        tree.inOrder(); // [20,30,40,50,60,70,80]
+		// tree.inOrderTraversal(tree.root); // [20,30,40,50,60,70,80]
+		tree.inOrder(); // [20,30,40,50,60,70,80]
 
-        System.out.println();
-        System.out.println(tree.search(tree.root,40)); // true
+		System.out.println();
+		System.out.println(tree.search(tree.root,40)); // true
 
-        System.out.println("Min Node: "+tree.minNode(tree.root)); // 20
-        System.out.println("Max Node: "+tree.maxNode(tree.root)); // 80
+		System.out.println("Min Node: "+tree.minNode(tree.root)); // 20
+		System.out.println("Max Node: "+tree.maxNode(tree.root)); // 80
 
-        tree.delete(50);
-        tree.inOrder(); // [20,30,40,60,70,80]
-	}
+		tree.delete(50);
+		tree.inOrder(); // [20,30,40,60,70,80]
+		}
 }
