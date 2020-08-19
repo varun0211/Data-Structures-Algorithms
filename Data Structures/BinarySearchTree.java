07265c22b59146f5e6db1993bd91a89aa22db983
varun0211 <r.varunrockz@gmail.com>
2020-08-20 01:38:16 +0530

    Update BinarySearchTree.java

--- Data Structures/BinarySearchTree.java
+++ Data Structures/BinarySearchTree.java
@@ -149,12 +149,12 @@ Note: Recursive insertion should be implemented(not yet tried)
 		BinarySearchTree tree = new BinarySearchTree();
 
 		tree.insert(50); 
-        	tree.insert(30); 
-        	tree.insert(20); 
-        	tree.insert(40); 
-        	tree.insert(70); 
-        	tree.insert(60); 
-        	tree.insert(80);
+		tree.insert(30); 
+		tree.insert(20); 
+		tree.insert(40); 
+		tree.insert(70); 
+		tree.insert(60); 
+		tree.insert(80);
 
 		// tree.inOrderTraversal(tree.root); // [20,30,40,50,60,70,80]
 		tree.inOrder(); // [20,30,40,50,60,70,80]
@@ -165,7 +165,7 @@ Note: Recursive insertion should be implemented(not yet tried)
 		System.out.println("Min Node: "+tree.minNode(tree.root)); // 20
 		System.out.println("Max Node: "+tree.maxNode(tree.root)); // 80
 
-		tree.delete(50);
+		tree.delete(40);
 		tree.inOrder(); // [20,30,40,60,70,80]
 		}
 }
